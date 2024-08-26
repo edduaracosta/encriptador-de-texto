5e1261fd9ae85772468fa990278fd30472dbe1b9
edduar <edduaracostax3@gmail.com>
2024-08-26 17:22:42 -0500

    enctiptar input

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-let textoAEncriptar = "";
-let textoEncriptado = "";
+
 let listaTextoEncriptado = [];
-let vocales = ["a","e","i","o","u"];
-let relleno = ["i", "nter","mes", "ber", "fati"]
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -11,20 +8,49 @@ function asignarTextoElemento(elemento, texto){
 }
 
 function encriptar(){
-    separar();
-}
-function separar(){
-    validar();
-    if(textoAEncriptar!=" "){
-        return;
-    }
+    let textoAEncriptar =  validar();
+    //cambiar de ambito la clave de encriptacion para mas seguridad
+    
+    //for(let i=0; i<textoAEncriptar.length(); i++){
+    //}
+    let textoEncriptado = procesar(textoAEncriptar);
+
+    console.log(textoEncriptado);
+
+    listaTextoEncriptado.push(textoEncriptado);
+
 }
 
+function procesar(texto) {
+    const relleno = {
+        "a":"i", 
+        "e":"nter",
+        "i":"mes", 
+        "o":"ber", 
+        "u":"fati",
+        "á":"i", 
+        "é":"nter",
+        "í":"mes", 
+        "ó":"ber", 
+        "ú":"fati"
+    }
+    //replace("a","i")
+    return texto.replace(/[aeiouáéíóú]/g, (vocal) => 
+    {
+        console.log(vocal);
+        return vocal + relleno[vocal.toLowerCase()];
+        
+    });
+}
 function validar(){
     let textoDeUsuario = document.getElementById("userValue").value;
     if(textoDeUsuario.match(/\p{Lu}|\p{N}|\p{S}|\p{P}/gu)){
-        alert("Recuerda NO usar mayusculas, numeros ni simbolos");
+        alert("Recuerda NO usar mayusculas, números ni simbolos"); 
+        return false;
     }
+    console.log(textoDeUsuario);
+    return textoDeUsuario;
+    
 }
 function limpiar(){
     return document.querySelector("#userValue").value = ""; ;
